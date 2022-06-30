e10cb7cdb4ceecfb40dccc4a637dd462f8f1c366
aditig23 <37783933+A-SG23@users.noreply.github.com>
2022-06-30 14:25:24 -0700

    set up Display file, added JavaFX Library

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Display {
+public class Display extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
